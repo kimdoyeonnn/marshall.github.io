a0b831a279e3eb39b98e151055d7fcc02c5202e3
kimdoyeonnn <151764224+kimdoyeonnn@users.noreply.github.com>
2024-02-06 12:18:22 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+  $('.favorite_box').slick({
+    dots: false,
+    prevArrow : $('.prevArrow'), 
+    nextArrow : $('.nextArrow'), 
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+    
+  });
